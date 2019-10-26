78644a98e729f714e44b0eedd0a9a963ee9664fb
ookami.kb <ookami.kb@gmail.com>
2019-10-26 18:25:29 +0200

    Update results page for events

--- app_angular/lib/src/route_paths.dart
+++ app_angular/lib/src/route_paths.dart
@@ -1,11 +1,9 @@
 import 'package:angular_router/angular_router.dart';
-
+import 'package:app_angular/src/events/route_paths.dart';
 
 class RoutePaths {
   static final root = RoutePath(path: '');
   static final games = RoutePath(path: 'games');
   static final events = RoutePath(path: 'events');
-  static final results = RoutePath(path: 'results');
+  static final results = RoutePath(path: 'results/:$eventIdParam');
 }
-
-
